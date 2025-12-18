f77477dcfed9fa2528d68ec0168eb04f3a44c20f
Diar Gashi <msgwxf7mmk@privaterelay.appleid.com>
2025-12-18 18:45:55 +0100

    Add files via upload

---
+++ ArrayTesting.java
@@ -0,0 +1,88 @@
+package Beginn;
+
+
+
+public class ArrayTesting {
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		int array[] = {1,2,3,4,5,6};
+		
+		ArrayMath(array);
+		
+		System.out.println(Quersumme(111,0));
+		
+		System.out.println(Counting(11111));
+		
+	
+		
+		
+		
+		
+	}
+	
+	
+	public static int Counting(int num) {
+		if (num !=0) {
+			
+			return 1 + Counting(num/10);}
+		return 0;
+		
+	}
+	
+	public static int Quersumme(int h, int start) {
+		if (h !=0) {
+			start += h%10;
+			return Quersumme(h/10,start);
+		}
+		return start;
+		
+		
+	}
+	
+	
+	
+	public static void ArrayMath(int [] array) {
+		int sum = 0;
+		
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+			
+		}
+		
+		System.out.println("Der Durschnitt ist : " + (double)sum/array.length);
+		int min = array[0];
+		int max = array[0];
+		
+		for (int i = 1; i < array.length; i++) {
+			if (min>array[i]) {
+				min = array[i];
+				
+			}
+			if (max<array[i]) {
+				max = array[i];
+				
+			}
+		}
+		System.out.println("Minimimum = " + min + " Maximum = " + max);
+		
+		double median ;
+		if (array.length%2==0) {
+			median = (array[array.length/2]+array[array.length/2-1])/2.0;
+		}
+		else {median = array[array.length/2];}
+		System.out.println("Median = " + median);
+		
+	}
+	
+	
+	
+	
+	
+	
+}
+
---
+++ MagicSquare.java
@@ -0,0 +1,93 @@
+package Beginn;
+
+
+
+//Magisches Quadrat programmed on Eclipse IDE
+public class MagicSquare {
+	
+	public static boolean MagicSquareCheck(int [][]arr) 
+	{
+		int sum = 0;
+	
+	int start = arr[0][0];
+	
+		if (arr.length != arr[0].length){
+			return false;
+		}
+		for (int i = 0; i<arr.length;i++) {
+			sum += arr[0][i];
+		}
+		if (!CheckSumZeile(arr,sum)) {
+			return false;
+		}
+		if (!CheckSumSpalte(arr,sum)) {
+			return false;
+		}if (!CheckDiagonal(arr,sum)) {
+			return false;
+		}
+		return true;
+		
+		
+		
+	}
+			
+	public static boolean CheckSumZeile(int arr [][], int sum)	{
+		int tempsum;
+		boolean temp = true;
+		for (int i = 1; i<arr.length;i++) {
+			tempsum = 0;
+			for (int j = 0;j<arr.length;j++) {
+				tempsum += arr[i][j];
+				
+				
+			}
+			if (sum!=tempsum){temp = false;}
+			
+		}
+		return temp;
+	}
+	
+	public static boolean CheckDiagonal(int arr[][] ,int sum) {
+		
+		int tempsum = 0;
+		int tempsum2 = 0;
+		for (int i = 0; i < arr.length; i++) {
+			 
+				tempsum+=arr[i][i];	
+				tempsum2 += arr[i][arr.length-1-i];
+				
+			}
+		
+		if (tempsum != tempsum2 || sum!=tempsum) {
+			return false;
+		}
+		return true;
+	}
+	public static boolean CheckSumSpalte(int arr [][], int sum)	{
+		int tempsum;
+		boolean temp = true;
+		for (int i = 0; i<arr.length;i++) {
+			tempsum = 0;
+			for (int j = 0;j<arr.length;j++) {
+			tempsum += arr[j][i];
+				
+				
+			}
+			if (sum!=tempsum){temp = false;}
+			
+		}
+		return temp;
+	}
+	
+	
+	public static void main(String[] args) {
+	int [][] arr = {{1,1,1},{1,1,1},{1,1,1}};
+	System.out.print(MagicSquareCheck(arr));
+		
+	
+	
+	
+		
+	}
+
+}
