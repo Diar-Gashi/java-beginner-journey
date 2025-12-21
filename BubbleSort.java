957962c3a26c5ecb266b2ad81b24b8e65ea45e63
Diar Gashi <msgwxf7mmk@privaterelay.appleid.com>
2025-12-21 12:38:45 +0100

    camelCase BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,7 +5,7 @@ package Beginn;
 public class BubbleSort {
 	
 	//Reversing the Array : O(n)
-	public static int[] ReverseArray(int [] array, int start, int end) {
+	public static int[] reverseArray(int [] array, int start, int end) {
 		
 		int temp;
 		if (end - start > 0) {
@@ -13,7 +13,8 @@ public class BubbleSort {
 			temp = array[start];
 			array[start] = array[end];
 			array[end] = temp;
-			return ReverseArray(array,start+1,end-1);
+			
+			return reverseArray(array,start+1,end-1);
 		}
 		
 		return array;
@@ -21,11 +22,12 @@ public class BubbleSort {
 	
 	
 	//Sorting the Array: O(n^2)
-	public static int[] BubbleSorting(int [] array) {
+	public static int[] bubbleSorting(int [] array) {
 		
 		int temp;
 		
 		for (int i = 0; i < array.length; i++) {
+			
 			for (int j = 0; j < array.length-1; j++) {
 				
 				if (array[j]> array[j+1]) {
@@ -50,17 +52,19 @@ public class BubbleSort {
 		//unsorted array to test the methods
 		int [] test = {1,7,3,9,8};
 		
-		test = ReverseArray(test,0,test.length-1);
+		test = reverseArray(test,0,test.length-1);
 		
 		//Output for Reversed Array
 		for (int i = 0; i < test.length; i++) {
+			
 			System.out.print(test[i] + " ");
 		}
 		System.out.println();
 		
 		//Sorting the Array with Bubble Sort
-		test = BubbleSorting(test);
+		test = bubbleSorting(test);
 		for (int i = 0; i < test.length; i++) {
+			
 			System.out.print(test[i] + " ");
 		}
 		
