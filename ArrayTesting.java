35f4cbe584f1cc8d29b6c446ef8533a73d23f54c
Diar Gashi <msgwxf7mmk@privaterelay.appleid.com>
2025-12-21 12:38:10 +0100

    camelCase ArrayTesting.java

--- ArrayTesting.java
+++ ArrayTesting.java
@@ -1,22 +1,22 @@
 package Beginn;
 
 
-//mathematical tesing with arrays and recursive methods
+
 public class ArrayTesting {
 	
+	//Array-Codetesting
 	
 	
 	
-	//Test-Main output on Console
 	public static void main(String[] args) {
-		
+		//sorted Array
 		int array[] = {1,2,3,4,5,6};
 		
-		ArrayMath(array);
+		arrayMath(array);
 		
-		System.out.println(Quersumme(111,0));
+		System.out.println(crossSum(111,0));
 		
-		System.out.println(Counting(11111));
+		System.out.println(countingNum(11111));
 		
 	
 		
@@ -25,20 +25,24 @@ public class ArrayTesting {
 		
 	}
 	
-	//counting the digits from an Integer
-	public static int Counting(int num) {
+	//counting the digits
+	public static int countingNum(int num) {
+		
 		if (num !=0) {
 			
-			return 1 + Counting(num/10);}
+			return 1 + countingNum(num/10);}
 		return 0;
 		
 	}
-
-	//Method for the digital root from an Integer
-	public static int Quersumme(int h, int start) {
+	
+	//cross sum from integer
+	
+	public static int crossSum(int h, int start) {
+		
 		if (h !=0) {
+			
 			start += h%10;
-			return Quersumme(h/10,start);
+			return crossSum(h/10,start);
 		}
 		return start;
 		
@@ -46,25 +50,31 @@ public class ArrayTesting {
 	}
 	
 	
-	//median, average, smallest and highest number with sorted arrays
-	public static void ArrayMath(int [] array) {
+	//Method for output for avg, median, highest und lowest integer
+	public static void arrayMath(int [] array) {
+		
 		int sum = 0;
 		
 		for (int i = 0; i < array.length; i++) {
+			
 			sum += array[i];
 			
 		}
 		
 		System.out.println("Der Durschnitt ist : " + (double)sum/array.length);
+		
 		int min = array[0];
 		int max = array[0];
 		
 		for (int i = 1; i < array.length; i++) {
+			
 			if (min>array[i]) {
+				
 				min = array[i];
 				
 			}
 			if (max<array[i]) {
+				
 				max = array[i];
 				
 			}
@@ -72,10 +82,14 @@ public class ArrayTesting {
 		System.out.println("Minimimum = " + min + " Maximum = " + max);
 		
 		double median ;
+		
+		//looking for the median in sorted arrays in 2 cases
 		if (array.length%2==0) {
+			
 			median = (array[array.length/2]+array[array.length/2-1])/2.0;
 		}
 		else {median = array[array.length/2];}
+		
 		System.out.println("Median = " + median);
 		
 	}
